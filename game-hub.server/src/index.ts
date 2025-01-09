1d69fed3bfb295cb27fba0cd13037be386857b79
Rusheel <rusheel@caredocs.ai>
2025-01-09 14:54:31 -0800

    started server code properly

--- game-hub.server/src/index.ts
+++ game-hub.server/src/index.ts
@@ -1,6 +1,16 @@
+import { createServer } from 'node:http';
+
 function main(): void {
-    console.log('Hello, world!');
-    console.log("Hello 2");
+    const hostname = '127.0.0.1';
+    const port = 8000;
+    const server = createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
+    });
+    server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+    });
 }
 
 main();
